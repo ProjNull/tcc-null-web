caca52f669ceef52537fa17744695e2769f96255
Jan Palma <honzik@janpalma.cz>
2025-10-03 17:34:55 +0200

    Added Mobile menu

--- themes/tcc-null/assets/js/main.js
+++ themes/tcc-null/assets/js/main.js
@@ -1,9 +1,15 @@
 function setupMobileMenu() {
-    const menuOpen = document.getElementById("main-menu-open");
-    const menuClose = document.getElementById("main-menu-close");
-    const menu = document.getElementById("main-menu");
+  const menuOpen = document.getElementById("main-menu-open");
+  const menuClose = document.getElementById("main-menu-close");
+  const menu = document.getElementById("main-menu");
 
-    menuOpen.addEventListener("click", () => {
-        
-    })
+  menuOpen.addEventListener("click", () => {
+    menu.classList.add("open");
+  });
+
+  menuClose.addEventListener("click", () => {
+    menu.classList.remove("open");
+  });
 }
+
+addEventListener("DOMContentLoaded", setupMobileMenu);
